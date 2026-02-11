e0c69a22315164f1285e4207d308f7a9ad067970
Dennis Jonathan <dennisjonathan78@gmail.com>
2026-02-11 15:34:55 +0700

    fix(blog): add type assertions to src props

--- components/blogs/article/Markdown/MarkdownComponents.tsx
+++ components/blogs/article/Markdown/MarkdownComponents.tsx
@@ -101,10 +101,10 @@ export const markdownComponents: Components = {
 
         return (
             <span className="block my-8 group">
-                <PhotoView src={src}>
+                <PhotoView src={src as string}>
                     <span className="relative block rounded-xl overflow-hidden border border-border cursor-zoom-in">
                         {/* Noise Overlay */}
-                        <span 
+                        <span
                             className="absolute inset-0 z-10 pointer-events-none opacity-15 mix-blend-overlay"
                             style={{
                                 backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`
@@ -157,7 +157,7 @@ export const markdownComponents: Components = {
                         poster={poster}
                         {...rest}
                     >
-                        {src ? <source src={src} /> : null}
+                        {src ? <source src={src as string} /> : null}
                         {children}
                     </video>
                 </div>
@@ -229,7 +229,7 @@ export const markdownComponents: Components = {
                         muted={muted}
                         {...rest}
                     >
-                        {src ? <source src={src} /> : null}
+                        {src ? <source src={src as string} /> : null}
                         {children}
                     </audio>
                 </div>
